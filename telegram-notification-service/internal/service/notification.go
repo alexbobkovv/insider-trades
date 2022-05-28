21a0fa8d9f5e2d98bcfdb750ceee8dd6fd6696b1
Alexander <AlexanderBobkov@outlook.com>
2022-05-28 05:05:38 -0400

    add(tg-ntfc): min total value constraint

--- telegram-notification-service/internal/service/notification.go
+++ telegram-notification-service/internal/service/notification.go
@@ -15,6 +15,11 @@ func New(tgAPI TelegramAPI) *notificationService {
 }
 
 func (n *notificationService) ProcessTrade(trade *api.Trade) error {
+	const minTotalValue = 10000
+	if trade.Trs.TotalValue < minTotalValue {
+		return nil
+	}
+
 	if err := n.api.SendTrade(trade); err != nil {
 		return fmt.Errorf("service: ProcessTrade: %v", err)
 	}
